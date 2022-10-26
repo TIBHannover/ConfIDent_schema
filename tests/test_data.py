5770acf63b18e0fe2db0ffd4f7dd1040b5ac688b
Philip Strömert <Philip.Stroemert@tib.eu>
2022-10-26 13:40:35 +0200

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,7 @@ import glob
 import unittest
 
 from linkml_runtime.loaders import yaml_loader
-from src.ConfIDent_schema.datamodel import ConfIDentRecords
+from ConfIDent_schema.datamodel import ConfIDentRecords
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
